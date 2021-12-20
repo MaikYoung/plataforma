682d793c8f167a1f774d53b808ec3c32689e50d5
Mike Young <maikelym1990@gmail.com>
2021-12-20 18:47:46 +0100

    added models without migrations

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(blank=True, max_length=250)
+    number = models.CharField(blank=True, max_length=250)
+    address = models.CharField(blank=True, max_length=250)
+    text = models.CharField(blank=True, max_length=250)
+    create_at = models.DateTimeField(auto_now_add=True)
